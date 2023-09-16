1d4e2dc56f2de91182720933ca59d7d535245634
varshil1 <shahvarshil@yahoo.com>
2023-09-16 11:41:13 -0700

    Hey world

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HashRouter as Router } from "react-router-dom";
 
-import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
-  <BrowserRouter>
+  <Router>
     <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </Router>,
   document.getElementById('root')
 );
